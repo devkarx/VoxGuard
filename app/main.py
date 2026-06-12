f185d6338c994824e32838877e02a532cf4f5d07
devkarx <aradhydeokar@gmail.com>
2026-06-12 10:25:29 +0000

    minor fix

--- app/main.py
+++ app/main.py
@@ -22,6 +22,9 @@ import tempfile
 # Allow importing from the project root
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="transformers")
+
 
 # ---------------------------------------------------------------------------
 # Page setup
